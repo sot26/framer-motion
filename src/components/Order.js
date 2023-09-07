41bd536d28cee70531ed6896f69e6a8ebafd3874
sot26 <omotoshotobi26@gmail.com>
2023-09-07 20:59:50 +0100

    framer-motion variants

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import { motion } from "framer-motion";
+import React from "react";
+
+const containerVariant = {
+  hidden: {
+    x: "100vw",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const childVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div
+      className="container order"
+      variants={containerVariant}
+      initial="hidden"
+      animate="visible"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+      <motion.p variants={childVariant}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariant}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default Order;
